64435a574a4a240fd4e77594981df77064a18613
Agnieszka <agnieszka.sekular@gmail.com>
2019-05-14 23:56:24 +0200

    2.A cd

--- 2.A/main.js
+++ 2.A/main.js
@@ -3,13 +3,25 @@ square.classList.add('box');
 document.body.appendChild(square);
 let size = 10;
 square.style.height = size + "px";
-square.style.width = size + "px"; 
-// konkatenacja/string
+square.style.width = size + "px"; // konkatenacja/string
+
+let grow = true; // flaga
 
 window.addEventListener("scroll", function(){
-    if (size < window.innerWidth / 2){
+    
+    if (size >= window.innerWidth / 2) {
+        grow = !grow;
+    } else if (size === 0) {
+        grow = true;
+    }
+
+    if (grow){ // true 
         size += 10;
         square.style.width = size + "px";
         square.style.height = size + "px";
-    }
+    } else {
+        size -= 10;
+        square.style.width = size + "px";
+        square.style.height = size + "px";
+    }  
 })
